508051dbb2817e8709533ad2768fe6eb782513ac
Achilles Enam <100866041+jsejava@users.noreply.github.com>
2023-03-12 23:50:46 +0000

    complet apps

---
+++ api/routes/rooms.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRooms,
+  getRoom,
+  updateRoom,
+  updateRoomAvailabiltity,
+} from "../controllers/room.js";
+
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+router.put("/availability/:id", updateRoomAvailabiltity);
+
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//GET
+router.get("/:id", getRoom);
+
+//GET ALL
+router.get("/", getRooms);
+export default router;
